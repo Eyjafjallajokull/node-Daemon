9a1dfc2675dc901885d502f8e7c1630617085573
Eyjafjallajokull <pawel.olejniczak@gmail.com>
2010-05-08 12:54:13 +0200

    complete rewrite

--- d.js
+++ d.js
@@ -1,127 +1,94 @@
 var sys = require('sys'),
   fs = require('fs'),
   spawn = require('child_process').spawn,
-  path = require('path'),
-  args = parse(process.argv),
-  intro = args.intro,
-  dir = args.dir,
-  test = args.test;
+  path = require('path');
+
+
+function Daemon (args) {
+  this.whatchedFiles = [];
+  this._depQuene = [];
+  this._process = null;
   
-playlist(intro, function(err, list) {
-  if(err) throw err;
-  if(!list.length) throw new Error("No files in the playlist.");
-  say('"Party time!"');
-  var playing = play(intro);
+  // parse args
+  this.childName = args[2];
+  if (!args[2]) {
+    sys.puts("Usage: node d.js server.js [params]");
+    process.exit(1);
+  }
+  args.splice(0,2);
+  this.childParams = args;
   
-  // Watch changes on any files of the playlist
-  list.forEach(function(file) {
+  this.collectDeps(this.childName);
+}
+Daemon.prototype.collectDeps = function (file) {
+  this._depQuene.push(file);
+  this.whatchedFiles.push(file);
+  this._collectDeps();  
+}
+
+Daemon.prototype._collectDeps = function () {
+  var file = this._depQuene.pop();
+  
+  var d = this;
+  fs.readFile( file, function(err, data) { 
+    var deps = {};
+    var dir = path.dirname(file);
+    
+    while(match = /(?:^|[^\w-])require *\(\s*['"](\.\/|\.\.|\/)(.*?)['"]\s*\)/g.exec(data)) {
+      var t = path.join(match[1] == '/'? '' : dir , (match[1] != "./"? match[1] : '') + match[2] + '.js');
+      if (d.whatchedFiles.indexOf(t)==-1)
+        deps[t] = true;
+    }
+    deps = Object.keys(deps);        
+    deps.forEach(function(dep) {
+      d._depQuene.push(dep);
+      d.whatchedFiles.push(dep);
+    });
+    
+    if (d._depQuene.length==0)
+      d.startWatching();
+    else
+      d._collectDeps();
+  });
+}
+Daemon.prototype.startWatching = function(argv) {
+  var d = this;
+  this.whatchedFiles.forEach(function(file) {
     fs.watchFile(file, function(curr, prev) {
-      say("playlist updated");
-      stop(playing, intro);
-      playing = play(intro);
+      d.restartChild();
     });
   });
-  
-  function exit() {
-    playing = null;
-    sys.puts("Process ended.");
-    sys.puts("Press [enter] to restart.");
-    var stdin = process.openStdin();
-    stdin.setEncoding('utf8');
-    stdin.addListener('data', function (chunk) {
-      if (chunk[chunk.length-1]=="\n")
-        playing = play(intro);
-    });
-  }
-  function play(intro) {
-    //say("start playing "+intro);
-    var playing = spawn('node', [intro]);
-    playing.stdout.addListener('data', function(data) {
-      sys.print(data);
-    });
-    playing.stderr.addListener('data', function(data) {
-      sys.print(data);
-    });
-    playing.addListener('exit', exit );
-    return playing;
-  }
-  function stop(playing, intro) {
-    if(playing) {
-      playing.kill();
-      //say("stop playing "+intro);
-    }
-  }
+  this.startChild();
+}
+Daemon.prototype.startChild = function() {  
+  this._process = spawn('node',this.childParams);
+  this._process.stdout.addListener('data', function(data) {
+    sys.print(data);
+  });
+  this._process.stderr.addListener('data', function(data) {
+    sys.print(data);
+  });
+  var d = this;
+  //this._process.addListener('exit', function() {});
+}
+Daemon.prototype.killChild = function() {
+  if (this._process && this._process.pid) this._process.kill();
+}
+Daemon.prototype.restartChild = function() {
+  sys.puts("\033[31mRestarting child process.\033[m");
+  this.killChild();
+  this.startChild();
+}
+
+sys.puts("Press [enter] anytime to restart child process.");
+sys.puts("Press Ctrl+C to exit node-Daemon.");
+var stdin = process.openStdin();
+stdin.setEncoding('utf8');
+stdin.addListener('data', function (chunk) {
+  if (chunk[chunk.length-1]=="\n")
+    d.restartChild();
 });
 
-function playlist(file, callback) {
-  var directory = path.dirname(file);
-    // Add potentially missing extension
-  if(file && !file.match(/\.js$/)) { file+= '.js'; }
-  fs.readFile(file, function(err, data) {
-    if(err) {
-      if(file) say('"Can\'t get my hands on '+ intro +'"');
-      return callback.apply(global, [null, []]);
-    }
-    
-    // We are using hashs to collect lists of _unique_ files
-    var list = {}, deps = {},
-      depsNb, i, read = 0;
-    
-    // Add the current file
-    list[file] = true;
-    
-    // Search dependencies of that file
-    while(match = /(?:^|[^\w-])require *\(\s*['"](\.\/|\.\.|\/)(.*?)['"]\s*\)/g.exec(data)) {
-      deps[path.join(match[1] == '/'? '' : directory , (match[1] != "./"? match[1] : '') + match[2] + '.js')] = true;
-    }
-    deps = Object.keys(deps);
-    
-    i = depsNb = deps.length;
-    // Make sure that the dependency exists and recursively search its dependencies;
-    do {
-      playlist(deps[--i] || '', function(err, _list) {
-        if(err) throw err;
-        // Add dependencies to the list
-        _list.forEach(function(value) {
-          list[value] = true;
-        });
-        // Return list when the last file has been inspected
-        if(++read >= depsNb) {
-          callback.apply(global, [null, Object.keys(list)]);
-        }
-      });
-    } while(i > 0);
-  });
-}
+var d = new Daemon(process.argv);
 
-function parse(argv) {
-  var intro, dir, test;
-  intro = argv[2];
-  // Add potentially missing extension
-  intro = intro? intro + (intro.match(/\.js$/)? '' : '.js') : 'intro.js';
-  
-  if(argv.indexOf('-d') != -1) {
-    dir = argv[argv.indexOf('-d') +1];
-  } else if(argv.indexOf('--dir') != -1) {
-    dir = argv[argv.indexOf('--dir') +1];
-  }
-  
-  if(argv.indexOf('-t') != -1) {
-    test = argv[argv.indexOf('-t') +1] || true;
-  } else if(argv.indexOf('--test') != -1) {
-    test = argv[argv.indexOf('--test') +1] || true;
-  }
-  
-  return {
-    intro: intro,
-    dir: dir,
-    test: test
-  }
-}
 
-global.say = function(message) {
-  sys.puts('DJs: '+message);
-}
-
-exports.playlist = playlist;
-exports.parse = parse;
